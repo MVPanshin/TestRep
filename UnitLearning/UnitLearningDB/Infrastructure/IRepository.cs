bd1feebfed20be11b39dfb5654ea1908460f9fcc
MVPanshin <maxim.panshin@unitgroup.ru>
2016-04-09 18:03:16 +0500

    IRepository add new methods

--- UnitLearning/UnitLearningDB/Infrastructure/IRepository.cs
+++ UnitLearning/UnitLearningDB/Infrastructure/IRepository.cs
@@ -8,17 +8,59 @@ namespace UnitLearningDB
 {
     public interface IRepository
     {
+        #region Курсы и все что с ними связано
         // Получение списка курсов
         IEnumerable<Course> GetAllCources();
         IEnumerable<Course> GetAllCourcesByDirection(Direction direct);
 
+        // Список курсов на которых отучился Слушатель/Компания
+        IEnumerable<Course> GetAllCourcesForStudent(int studentID);
+        IEnumerable<Course> GetAllCourcesForCompany(int companyID);
+
         // Получение курса по коду
         Course GetCourceByCode(string code);
 
+        // DML операции с курсами
+        void AddOrUpdateCourse(Course course);
+        void DeleteCourse(Course course);
+
+        void UpdateCoursePrice(decimal newPrice, params int[] courseID);
+        #endregion
+
+        #region Расписание
+        // Расписание курсов за разное время
+        IEnumerable<Shedule> GetSheduleForCurrentYear();
+        IEnumerable<Shedule> GetSheduleForCustomYear();
+        IEnumerable<Shedule> GetSheduleForCurrentMonth();
+        IEnumerable<Shedule> GetSheduleForCustomMonth();
+        IEnumerable<Shedule> GetSheduleForCurrentWeek();
+        IEnumerable<Shedule> GetSheduleForCustomWeek();
+        IEnumerable<Shedule> GetSheduleForRange(DateTime start, DateTime end);
+
+        //Добавление курса в расписание
+        void AddOrUpdateCourseToShedule(Shedule shedule, DateTime startDate, DateTime? endDate = null, 
+                int? managerID = null, int? teacherID = null,
+                StatusCode statusCode = null, int state = 1, int fullState = 1);
+
+        //Изменение статуса курса в расписании
+        void ChangeStatusCourseToShedule(Shedule shedule, StatusEnum status);
+        #endregion
+
+        #region Работа с Составом УЦ
         // Список преподавателей
         IEnumerable<Teacher> GetAllTeacher();
+        Teacher GetTeacherByID(int teacherID);
 
-        // Расписание курсов
-        Shedule GetShedule();
+        void AddOrUpdateTeacher(Teacher teacher);
+        #endregion
+
+        #region Клиенты
+        IEnumerable<Company> GetCompanies();
+        Company GetCompanyById(int companyID);
+        void AddOrUpdateCompany(Company company);
+
+        IEnumerable<Student> GetStudents();
+        IEnumerable<Student> GetStudentsForCompany(int companyID);
+        #endregion
     }
 }
--- UnitLearning/UnitLearningDB/Models/LearnProcess/StatusCode.cs
+++ UnitLearning/UnitLearningDB/Models/LearnProcess/StatusCode.cs
@@ -1,9 +1,24 @@
 ﻿namespace UnitLearningDB
 {
+    // Запланированный, Идет набор, Снятый, Прошедший
+    public enum StatusEnum { Open, Planned, Closed, Cancelled, Transferred }
+
     public class StatusCode
     {
+        public StatusCode(StatusEnum status)
+        {
+            this._status = status;
+        }
+
         public int StatusCodeID { get; set; }
-        // Запланированный, Идет набор, Снятый, Прошедший
-        public string Status { get; set; }
+        private StatusEnum _status;
+        
+        public string Status
+        {
+            get
+            {
+                return this._status.ToString();
+            }
+        }
     }
 }
